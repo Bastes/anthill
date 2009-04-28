19dedd15a353e1902490d8d3d3952871dc4cb9dd
Michel Belleville <michel.belleville@gmail.com>
2009-04-28 22:49:27 +0200

    Improved the Vector class.

--- lib/vector.rb
+++ lib/vector.rb
@@ -1,32 +1,102 @@
 =begin rdoc
 =Simple vector class
+Supports most vector calculus :
+- addition and substractions (+ -)
+- product (*) scalar and vector's
+- length manipulations
+- angle manipulations (relative to [1, 0] counter-clockwise, multiple of Pi)
+Operations between 2 vectors can take arguments written in various forms :
+- Vector.new(1, 0) + Vector.new(2, 3)
+- Vector.new(4, -5) - [-6, 7]
 =end
 module Anthill
   class Vector
-    # coordinates
+    # Coordinates
     attr_reader :x, :y
 
-    def initialize x = 0, y = 0
-      @x = x.to_f
-      @y = y.to_f
-    end
-
-    def == other
-      @x == other.x and @y == other.y
+    # Vector initialization
+    def initialize *args
+      args = args.flatten
+      case args.length
+        when 0: coordiates = [0, 0] 
+        when 1: coordiates = [args[0].x, args[0].y]
+        when 2: coordiates = args
+        else raise ArgumentError.new
+      end
+      @x, @y = coordiates
     rescue
-      false
+      raise ArgumentError.new "#{args} doesn't describe a vector."
     end
 
-    def + other
+    # Vector comparisons
+    def == *args
+      other = self.class.convert(args)
+      @x == other.x and @y == other.y
+    end
+
+    # Vector likeness (proximity of coordinates with a precision rate)
+    def is_like? other, precision = 0.001
+      other = self.class.convert(other)
+      ((@x - precision)..(@x + precision)).include? other.x and
+      ((@y - precision)..(@y + precision)).include? other.y
+    end
+
+    # Vector addition
+    def + *args
+      other = self.class.convert(args)
       Vector.new @x + other.x, @y + other.y
     end
 
-    def - other
+    # Vector substraction
+    def - *args
+      other = self.class.convert(args)
       Vector.new @x - other.x, @y - other.y
     end
 
+    # Vector product
+    # Supports scalar by vector and vector by vector products.
+    def * *args
+      other = self.class.convert(args)
+      @x * other.x + @y * other.y
+    rescue
+      value = args.first
+      Vector.new(@x * value, @y * value)
+    end
+
+    # Vector resizing
+    def << value
+      self * (value / length)
+    rescue
+      Vector.new
+    end
+
+    # Vector's length
     def length
       Math.sqrt(@x**2 + @y**2)
+    end
+
+    # Angle modification
+    def / value
+      l = self.length
+      a = value * Math::PI
+      Vector.new(Math.cos(a) * l, Math.sin(a) * l)
+    end
+
+    # Vector's angle
+    def angle
+      return (@x < 0 ? 1 : 0) if @y == 0
+      return (@y < 0 ? -1.0 / 2 : 1.0 / 2) if @x == 0
+      return Math.atan(@y / @x) / Math::PI + ((@x > 0 ? 0 : 1) * (@y <=> 0))
+    end
+
+    private
+
+    # Converts given arguments in a vector (if necessary)
+    # The method is used by most vector's operators to get a proper vector
+    # out of various ways of writing arguments.
+    def self.convert *args # ::nodoc::
+      return args.first if args.first.is_a?(Vector)
+      Vector.new(args)
     end
   end
 end
--- test/vector_test.rb
+++ test/vector_test.rb
@@ -16,28 +16,70 @@ module Anthill
     end
 
     def test_equals
-      v1 = Vector.new 1, 3
-      v2 = Vector.new 1, 3
-      v3 = Vector.new 2, 3
-      assert_equal v1, v2
-      assert_not_equal v1, v3
+      v = Vector.new 1, 3
+      assert v == Vector.new(1, 3)
+      assert v == [1, 3]
+      assert v != Vector.new(2, 3)
+      assert v != [2, 3]
     end
 
-    def test_plus
+    def test_like
+      v = Vector.new 2, 5
+      assert ! v.is_like?([-1, 5])
+      assert v.is_like?([2, 5])
+      assert v.is_like?([2.001, 4.999])
+      assert ! v.is_like?([2, 4.998])
+      assert v.is_like?([2.1, 4.9], 0.1)
+      assert ! v.is_like?([2.2, 4.9], 0.1)
+    end
+
+    def test_addition
       v1 = Vector.new(2, 3) + Vector.new(-3, 5)
       v2 = Vector.new(-1, 8)
       assert_equal v2, v1
     end
 
-    def test_minus
+    def test_substraction
       v1 = Vector.new(4, -2) - Vector.new(1, 6)
       v2 = Vector.new 3, -8
       assert_equal v2, v1
     end
 
+    def test_product
+      v = Vector.new(1, -5)
+      assert_equal Vector.new(2, -10), v * 2
+      assert_equal 0, v * [5, 1]
+      assert_equal(-13, v * Vector.new(2, 3))
+    end
+
     def test_length
-      v = Vector.new 3, -4
-      assert_equal 5, v.length
+      assert_equal(5, Vector.new(3, -4).length)
+    end
+    
+    def test_resize
+      assert_equal(Vector.new(3, 4), Vector.new(6, 8) << 5)
+    end
+
+    def test_angle
+      assert_equal 0, Vector.new.angle
+      assert_equal 0, Vector.new(7, 0).angle
+      assert_equal 1, Vector.new(-2, 0).angle
+      assert_equal 1.0 / 2, Vector.new(0, 8).angle
+      assert_equal((-1.0 / 2), Vector.new(0, -5).angle)
+      assert_equal 1.0 / 2, Vector.new(0, 8).angle
+      assert_equal((-1.0 / 2), Vector.new(0, -5).angle)
+      assert_equal 1.0 / 4, Vector.new(6, 6).angle
+      assert_equal 3.0 / 4, Vector.new(-7, 7).angle
+      assert_equal((-1.0 / 4), Vector.new(3, -3).angle)
+      assert_equal((-3.0 / 4), Vector.new(-8, -8).angle)
+    end
+
+    def test_reorient
+      v = Vector.new(1, 0)
+      assert Vector.new(0, 1).is_like?(v / (1.0 / 2))
+      assert Vector.new(-1, 0).is_like?(v / 1)
+      assert Vector.new(0, -1).is_like?(v / (3.0 / 2))
+      assert Vector.new(1, 0).is_like?(v / 2)
     end
   end
 end
